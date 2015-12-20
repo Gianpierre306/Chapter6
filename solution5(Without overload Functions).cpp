76cde862e79f57cb1af9fd80e14e793a51c552d1
Gianpierre <gianpierre306@yahoo.com>
2015-12-20 02:21:10 -0600

    Update solution5(Without overload Functions).cpp

--- solution5(Without overload Functions).cpp
+++ solution5(Without overload Functions).cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
@@ -10,6 +11,7 @@ int main()
    
     
     getOption();
+    cout << fixed << showpoint << setprecision(2);
     displayPatientData();
     return 0;
 }
